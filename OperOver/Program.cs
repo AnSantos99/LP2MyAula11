c0a56a41379384c8d4b49cf278d68e85f3bf8235
AnSantos99 <an.santos95@hotmail.com>
2019-12-11 17:53:51 +0000

    Overloading operators

---
+++ OperOver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperOver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3 v1 = new Vector3(2, 2, 3);
+            Vector3 v2 = new Vector3(10, 33, 4);
+
+            Console.WriteLine(v1 > v2);
+
+        }
+
+        
+    }
+}
---
+++ OperOver/Vector3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperOver
+{
+    struct Vector3
+    {
+        
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public Vector3(float a, float b, float c) 
+        {
+            X = a;
+            Y = b;
+            Z = c;
+        }
+
+
+        public float Magnitude =>
+            (float)Math.Sqrt(X * X+ Y * Y + Z * Z);
+
+        public static bool operator >(Vector3 v1, Vector3 v2)
+            => v1.Magnitude > v2.Magnitude;
+
+        public static bool operator <(Vector3 v1, Vector3 v2)
+            => v1.Magnitude < v2.Magnitude;
+
+
+
+
+
+
+
+    }
+}
