fd7a29e76563061adce0994ff5aa7f34084948dc
AnSantos99 <an.santos95@hotmail.com>
2019-12-11 16:14:00 +0000

    Exercise Threads FrogsRace

---
+++ FrogRace/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(FrogRace);
+
+            Thread t2 = new Thread(FrogRace);
+
+            Thread t3 = new Thread(FrogRace);
+
+            t1.Start(1);
+            t2.Start(2);
+            t3.Start(3);
+
+            t1.Join(1);
+            t2.Join(2);
+            t3.Join(3);
+        }
+
+        public static void FrogRace(object obj) 
+        {
+            
+
+            int id = (int)obj;
+            Random rnd = new Random(id);
+
+            for (int i = 0; i < 10; i++) 
+            {
+                Thread.Sleep(rnd.Next(500));
+                Console.WriteLine($"Rã #{id} deu salto #{i + 1}");
+                
+            }
+        }
+    }
+}
